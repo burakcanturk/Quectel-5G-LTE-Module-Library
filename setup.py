c0c6696544d9f0279f1d12997535bf54d7fa7329
Burak Cantürk <burakcanturk12@gmail.com>
2025-01-14 00:50:44 +0300

    xyz

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
+
 setup(
     name="quectel_at",  # Projenizin adı (benzersiz olmalı)
     version="1.1.0",  # Versiyon numarası
@@ -10,10 +14,10 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/burakcanturk/Quectel-5G-LTE-Module-Library.git",
     packages=["QuectelAT"],
+    install_requires=parse_requirements("requirements.txt"),  # Eğer dış bağımlılıklar varsa buraya ekleyin
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pyserial"],  # Eğer dış bağımlılıklar varsa buraya ekleyin
 )
